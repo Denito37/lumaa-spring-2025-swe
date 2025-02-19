eec1a974fbbaadb478c6e7c4f08cf2c646ef4201
Denito37 <69406178+Denito37@users.noreply.github.com>
2025-02-19 16:36:40 -0500

    login endpoint logic created

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 import { Request, Response, NextFunction } from "express";
 
-const SECRET = process.env.JWT_SECRET||"secret";
+const SECRET:any = process.env.JWT_SECRET;
 
 
 const authenticate = (req: Request, res: Response, next: NextFunction)=>{
--- backend/src/routes/login.ts
+++ backend/src/routes/login.ts
@@ -4,10 +4,27 @@ import jwt from "jsonwebtoken";
 import User from "../models/User";
 
 const router = express.Router();
+const SECRET:any = process.env.JWT_SECRET
 
-router.post("/auth/register", async(req, res)=>{})
+router.post("/auth/register", async(req, res)=>{
+    const {username, password} = req.body
+    const hashedPassword = await bcrypt.hash(password,12)
+    const userExist = await User.findOne({where:{ username }})
+    if(userExist)
+        res.status(400).json({error:"Username is taken"})
+    const user = await User.create({username, password:hashedPassword})
+    res.status(201).json(user)
+})
 
-router.post("/auth/login", async(req, res)=>{})
-
+router.post("/auth/login", async(req, res)=>{
+    const {username, password} = req.body
+    const user = await User.findOne({where:{ username }})
+    if(!user)
+        return res.status(401).json({error:"user not found"})
+    if(!await bcrypt.compare(password, user.password))
+        return res.status(401).json({error:"Invalid password"})
+    const token = jwt.sign({id:user.id},SECRET,{expiresIn:"1h"})
+    res.json({ token })
+})
 
 export default router
