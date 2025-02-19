1fc89d4d5c3eb756555dfebf3d372d5eba8b387a
Denito37 <69406178+Denito37@users.noreply.github.com>
2025-02-19 12:29:03 -0500

    added data models for db

---
+++ backend/src/models/Task.ts
@@ -0,0 +1,21 @@
+import {DataTypes, Model} from 'sequelize'
+import { sequelize } from '../config/db'
+
+class Task extends Model{
+    id!: number
+    title!: string
+    description!: string
+    isComplete!:boolean
+    userId!:number
+}
+
+Task.init({
+    id:{type:DataTypes.INTEGER, autoIncrement:true, primaryKey:true},
+    title:{type:DataTypes.STRING, allowNull:false},
+    description:{type:DataTypes.STRING, allowNull:true},
+    isComplete:{type:DataTypes.BOOLEAN, defaultValue:false},
+    userId:{type:DataTypes.INTEGER, allowNull:false}
+},{sequelize, modelName: "task"})
+
+
+export default Task
---
+++ backend/src/models/User.ts
@@ -0,0 +1,18 @@
+import {  DataTypes, Model } from "sequelize";
+import { sequelize } from "../config/db";
+
+
+class User extends Model{
+    public id!: number
+    public username!: string
+    public password!: string
+}
+
+User.init({
+    id :{type:DataTypes.INTEGER, autoIncrement:true, primaryKey:true},
+    username:{type:DataTypes.STRING, unique:true, allowNull:false},
+    password:{type:DataTypes.STRING,allowNull:false},
+
+},{sequelize, modelName: "user"})
+
+export default User
